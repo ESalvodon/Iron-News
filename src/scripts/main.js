7d0c401b6457b60b3a680588657e766afe6d71ea
Eddy Salvodon <Esalvodon@Gmail.com>
2016-03-09 16:55:54 -0500

    Updated Changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,13 @@ import $ from 'jquery';
 import Backbone from 'backbone';
 import _ from 'underscore';
 
-$(() => {
-  // Ready!
+import Router from './router';
+
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
+
+$(function() {
+  window.router = new Router();
+  Backbone.history.start();
 });
